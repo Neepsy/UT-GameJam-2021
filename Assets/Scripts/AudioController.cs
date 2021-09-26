4c5238b730caf894df1fb57bb30889e104938297
figueroass27 <71303620+figueroass27@users.noreply.github.com>
2021-09-26 14:49:33 -0500

    pause script works but everything a little glitchy

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
